cbbd9c29e1600f4528f48148ef2e446618dc05b4
ArthurLPereira <arthurleandro50@gmail.com>
2026-03-24 15:47:49 -0300

    Adicionando tratamento de erros no controller de tarefas

--- backend/src/controllers/tarefas.controller.ts
+++ backend/src/controllers/tarefas.controller.ts
@@ -1,47 +1,96 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import type { PatchTarefas, TypesTarefas } from "../types/tarefas.types";
 import { criarTarefas, buscarTarefas, buscarTarefasId, atualizarTarefa, deletarTarefa } from "../repositories/tarefas.repository";
+import { Prisma } from "@prisma/client";
 
 
 export async function CriarTarefa(request: FastifyRequest<{ Body: TypesTarefas }>, reply: FastifyReply) {
-    const data = request.body;
+    try {
+        const data = request.body;
 
-    const tarefas = await criarTarefas(data);
+        const tarefas = await criarTarefas(data);
 
-    return reply.status(201).send({ message: "Criado com sucesso", data: tarefas });
+        return reply.status(201).send({ message: "Criado com sucesso", data: tarefas });
+    } catch (erro) {
+        console.error(erro)
+        return reply.status(500).send({ message: "Erro ao criar tarefa" });
+    }
 
 }
 
 export async function BuscarTarefas(request: FastifyRequest, reply: FastifyReply) {
-    const data = await buscarTarefas();
+    try {
+        const data = await buscarTarefas();
 
-    return reply.status(200).send({ data: data });
+        if (data.length === 0) {
+            return reply.status(404).send({ message: "Não existe tarefas criadas" });
+        }
+
+        return reply.status(200).send({ data: data });
+    } catch (erro) {
+        console.error(erro)
+        return reply.status(500).send({ message: "Erro interno no servidor" });
+    }
 }
 
 export async function BuscarTarefaId(request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply) {
-    const idString = request.params.id;
-    const idNumber = Number(idString);
+    try {
+        const idString = request.params.id;
+        const idNumber = Number(idString);
+        const tarefa = await buscarTarefasId(idNumber);
 
-    const tarefa = await buscarTarefasId(idNumber);
+        if (!tarefa) {
+            return reply.status(404).send({ message: "Tarefa não encontrada" });
+        }
 
-    return reply.status(200).send({ data: tarefa });
+        return reply.status(200).send({ data: tarefa });
+    } catch (erro) {
+        console.error(erro)
+        return reply.status(500).send({ message: "Erro interno no servidor" });
+    }
 }
 
 export async function PatchTarefa(request: FastifyRequest<{ Params: { id: string }, Body: PatchTarefas }>, reply: FastifyReply) {
-    const idString = request.params.id;
-    const idNumber = Number(idString);
-    const data = request.body;
 
-    const tarefa = await atualizarTarefa(idNumber, data);
+    try {
+        const idString = request.params.id;
+        const idNumber = Number(idString);
 
-    return reply.status(200).send({ message: "Tarefa atualizada com sucesso", data: tarefa });
+        const data = request.body;
+
+        const tarefa = await atualizarTarefa(idNumber, data);
+
+        return reply.status(200).send({ message: "Tarefa atualizada com sucesso", data: tarefa });
+    } catch (erro) {
+        if (erro instanceof Prisma.PrismaClientKnownRequestError) {
+            if (erro.code == 'P2025') {
+                return reply.status(404).send({ message: "Tarefa não encontrada" });
+            }
+        }
+        console.error(erro)
+        return reply.status(500).send({ message: "Erro interno no servidor" });
+
+    }
 }
 
 export async function DeleteTarefa(request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply) {
-    const idString = request.params.id;
-    const idNumber = Number(idString);
+    try {
+        const idString = request.params.id;
+        const idNumber = Number(idString);
 
-    const tarefa = await deletarTarefa(idNumber);
+        const tarefa = await deletarTarefa(idNumber);
 
-    return reply.status(204).send();
+        return reply.status(204).send();
+
+    } catch (erro) {
+        if (erro instanceof Prisma.PrismaClientKnownRequestError) {
+            if (erro.code == 'P2025') {
+                return reply.status(404).send({ message: "Tarefa não encontrada" });
+            }
+        }
+        console.error(erro)
+        return reply.status(500).send({ message: "Erro interno no servidor" });
+
+    }
+
 }
